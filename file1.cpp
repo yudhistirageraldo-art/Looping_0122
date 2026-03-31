19a31d9bfcdf575776e6292fc67b0a6f44244081
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-03-31 20:34:34 +0700

    peluang

--- file1.cpp
+++ file1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main(){
     int x;
+
+    cout<<"PELUANG FOR"<<endl
+    for(int i=0; i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    srand(time(0));
+
     
 }
