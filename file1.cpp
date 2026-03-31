11952db2a7bee41e4541cc3feb3c7883fc0d3f43
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-03-31 21:20:20 +0700

    memasukkan logika OR

--- file1.cpp
+++ file1.cpp
@@ -44,4 +44,35 @@ int main(){
     cin>>nilM
     cout<<"memasukkan nilai bahasa inggris = ";
     cin>>nilB
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60) )
+        status = "Lulus"
+    else
+        status = "Tidak Lulus";
+
+    cout<<"status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+logika OR
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"memasukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"memasukkan nilai bahasa inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus"
+    else
+        status = "Tidak Lulus";
+    
+        cout<<"status kelulusan = "<<status<<" ,dengan niali rata-rata = "<<rerata<<endl;
 }
